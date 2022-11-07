197a34224deef9c42fc6c99b2e188a596a38a485
Garimahdi <g.astaghfari@gmail.com>
2022-11-07 10:28:47 +0700

    YAB SMP mockup v1

---
+++ js/script.js
@@ -0,0 +1,54 @@
+// Get the current year
+let getAnno = document.getElementById('anno');
+let getDate = new Date().getFullYear();
+console.log(getDate);
+getAnno.innerHTML=String(getDate);
+
+// Set a logic to adjust date
+
+let tanggal = $('#tanggal');
+let bulan = $('#bulan');
+let tahun = $('#tahun');
+console.log(bulan.val());
+console.log($('#bulan option:selected').html());
+
+bulan.change(function(event){
+    let bulanFix = $('#bulan option:selected').html();
+    console.log(bulanFix);
+
+    if(bulan.val() == 'apr' || bulan.val() == 'jun' || bulan.val() == 'sep' || bulan.val() == 'nov'){
+        $("#tanggal option[value='31']").hide()
+    }else if(bulan.val() == 'feb'){
+        $('#tanggal option[value="31"]').hide();
+        $('#tanggal option[value="30"]').hide();
+        $('#tanggal option[value="29"]').hide()
+    }else{
+        $('#tanggal option[value="31"]').show();
+        $('#tanggal option[value="30"]').show();
+        $('#tanggal option[value="29"]').show()
+    };
+});
+
+tahun.change(function(event){
+    console.log(Number($('#tahun option:selected').html()));
+    
+    if((tahun.val()%4 == 0) && (bulan.val() == 'feb')){
+        //console.log('yeet');
+        //tanggal.append('<option value='+29+'>'+29+"</option>")
+        $('#tanggal option[value="29"]').show();
+    }else if((tahun.val()%4 != 0) && (bulan.val() == 'feb')){
+        $('#tanggal option[value="29"]').remove();
+    }
+});
+
+// Adding clock
+let hour = Array.from({length:24},(v,k)=>k+1);
+let min = Array.from({length:60},(v,k)=>k+1)
+
+for(i in hour){
+    document.getElementById('jam').innerHTML += '<option value ="'+i+'">'+i+'</option>'
+};
+
+for(j in min){
+    document.getElementById('menit').innerHTML += '<option value ="'+j+'">'+j+'</option>'
+};
